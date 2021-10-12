93f6f8466ff7f1da8908ccebbf3addd6928e0af0
minsangkimme <minsang.tech@gmail.com>
2021-10-12 21:53:16 +0900

    [update] 한줄 소개 수정

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -3,7 +3,7 @@ module.exports = {
   description: `김테디 블로그`,
   author: `[minsangkimme]`,
   introduction: `개발을 하면서 가장 흥미롭게 생각하는 점은, '사람들이 서비스를 이용할 때 어떻게 행동할까'입니다. 생활 밀착형 서비스에서
-  사용자의 행동을 예측해 많은 부분을 배려하고 그 부분들을 재미있는 요소로 바꾸어 사용자에게 제공하는 점을 추구합니다.`,
+  사용자의 행동을 예측해 많은 부분을 배려하고 그 부분들을 재미있는 요소로 바꾸어 사용자에게 제공하는 것을 즐깁니다.`,
   siteUrl: `https://github.com/minsangkimme/minsangkimme.github.io`, // Your blog site url
   social: {
     twitter: ``, // Your Twitter account
